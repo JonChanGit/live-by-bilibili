70eeffebb81845fda21612e2e406b6f67fed1cb5
JonChan <jonchan.hv@outlook.com>
2018-07-28 01:28:23 +0800

    引入模块测试

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        MyComponentsss
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class MyComponent extends Vue{
+    
+}
+
+</script>
+
+<style scoped lang="less">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,18 @@
   <div class="home">
     <img src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <MyComponent/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import MyComponent from '@/components/MyComponent.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
+    HelloWorld,MyComponent
   },
 })
 export default class Home extends Vue {}

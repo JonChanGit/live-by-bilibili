3ee050500e39cb80d6c465b5424aa9edf422ed9c
JonChan <chenjunlai@ffcs.cn>
2018-07-28 16:12:02 +0800

    。。

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -1,17 +1,44 @@
 <template>
     <div>
-        MyComponentsss
+        {{msg}}
+        <video id="videoElement"></video>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import flvjs from 'flv.js'
 
-@Component
+@Component({
+    data(){
+        return {
+            msg:"@Component"
+        }
+    },
+    methods:{
+        
+    }
+})
+
+
 export default class MyComponent extends Vue{
-    
-}
+    mounted (){
+        this.loadPlayer();
+    }
 
+    loadPlayer(){
+        if (flvjs.isSupported()) {
+            let videoElement:any = document.getElementById('videoElement');
+            let flvPlayer = flvjs.createPlayer({
+                type: 'flv',
+                url: 'http://127.0.0.1/flv/7182741.json'
+            });
+            flvPlayer.attachMediaElement(videoElement);
+            flvPlayer.load();
+            flvPlayer.play();
+        }
+    }
+}
 </script>
 
 <style scoped lang="less">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
     <MyComponent/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import MyComponent from '@/components/MyComponent.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,MyComponent
+    MyComponent
   },
 })
 export default class Home extends Vue {}
